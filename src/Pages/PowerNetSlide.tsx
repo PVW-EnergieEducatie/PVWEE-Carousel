6a199f4f91605f473d78618baf42fd6087bc5616
Aaron <aaron.carton@student.howest.be>
2022-06-08 16:32:05 +0200

    Add animation and realtime data to Powernet slide

--- src/Pages/PowerNetSlide.tsx
+++ src/Pages/PowerNetSlide.tsx
@@ -1,6 +1,51 @@
+import { SVG } from '@svgdotjs/svg.js';
+import { useEffect, useState } from 'react';
 import { ReactComponent as Stroom } from '../assets/svg/EnergieStroom.svg';
+import BuildingArray from '../interfaces/BuildingData';
+import { getTransfoPowerData } from '../utils/data-acces';
 
-function SlideTwo() {
+function PowerNetSlide() {
+  const [realtimePower, setRealtimePower] = useState<BuildingArray>();
+
+  useEffect(() => {
+    // fetch realtime values every 6s and store in state
+    const fetchRealtimeData = () =>
+      getTransfoPowerData('total', 'realtime').then((buildings) => {
+        console.log('getting data');
+
+        delete buildings['Smappee']; // remove Smappee from data as it is not a building
+        setRealtimePower(buildings);
+      });
+
+    // execute right now first and after that every X seconds
+    fetchRealtimeData();
+    const realtimeInterval = setInterval(fetchRealtimeData, 1000 * 6); // 6s
+
+    return () => {
+      clearInterval(realtimeInterval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (realtimePower) {
+      console.log('setting svg', realtimePower);
+      let svg = SVG('#PowerNet');
+
+      let totalPower = 0;
+      for (const [key, building] of Object.entries(realtimePower)) {
+        let buildingPower = building.at(-1)?.value ?? 0;
+        totalPower += buildingPower;
+
+        let element = svg.findOne(`#${key}_powertext`);
+        if (element) {
+          element.node.textContent = `${buildingPower.toFixed(2)} kW`;
+        }
+      }
+      // set sum of all buildings
+      svg.findOne('#Transfo_powertext')!.node.textContent = `${totalPower.toFixed(2)} kW`;
+    }
+  }, [realtimePower]);
+
   return (
     <div className="embla__slide flex items-center justify-center bg-slate-200 p-12">
       <div className="flex h-full w-full items-center justify-center rounded-lg bg-white p-5 ">
@@ -10,4 +55,4 @@ function SlideTwo() {
   );
 }
 
-export default SlideTwo;
+export default PowerNetSlide;
