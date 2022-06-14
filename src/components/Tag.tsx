1d9fa98c4b207fac6ba7c2a1861627d938ffc667
DefoortMerijn <merijn.defoort@student.howest.be>
2022-06-14 14:42:21 +0200

    Edited styling

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,6 +1,6 @@
 export default ({ text }: { text: string }) => {
   return (
-    <p className="m-1 h-min whitespace-nowrap rounded bg-ondernemen-80 py-1.5 px-2.5 font-roboto text-sm text-white">
+    <p className="m-1 h-min whitespace-nowrap rounded bg-ondernemen-80 py-1.5 px-2.5 font-roboto text-base font-bold text-white">
       #{text}
     </p>
   );
